e6cac24033a3fac1cf04659de7fb7b79cc98fb15
user <user@192.168.254.1>
2013-04-15 17:02:48 -0300

    ...

--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorInt.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorInt.java
@@ -1,5 +1,6 @@
 package br.com.senacrs.alp.aulas;
 
+import java.lang.reflect.Array;
 import java.security.cert.CollectionCertStoreParameters;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,10 +13,9 @@ public class MeuComparatorInt implements Comparator<Integer>{
 	@Override
 	public int compare(Integer arg0, Integer arg1) {
 		
-		//Integer.valueOf(arg1).compareTo(Integer.valueOf(arg0));
 		
-		
-	 return arg0-arg1;	 
+		return arg1.compareTo(arg0);
+	 
 	}
 
 }
