ce761b425857c08d8a0f2fe28525f52192d37975
631220041 <631220041@Sala603-12.fspoaeduc.com.br>
2013-04-16 20:53:51 -0300

    ok

--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorInt.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorInt.java
@@ -8,14 +8,61 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import org.omg.CORBA.ARG_IN;
+
 public class MeuComparatorInt implements Comparator<Integer>{
 
 	@Override
 	public int compare(Integer arg0, Integer arg1) {
 		
+		int resultado = 0;
 		
-		return arg1.compareTo(arg0);
+		if (par(arg0) == par(arg1)) {
+			resultado = compareMesmaParidade(arg0, arg1);
+		} else{
+			resultado = compareParidadeDistinta(arg0, arg1);
+		}
+		
+		
+		
+		
+		return resultado;
 	 
+	}
+	
+	
+	public boolean par (Integer a){
+		
+		boolean resultado = false;
+		
+		resultado = a.intValue() %2 ==0;
+		
+		return resultado;
+		
+			
+	}
+	
+	private int compareMesmaParidade( Integer arg0, Integer arg1) {
+		
+		int resultado = 0;
+		
+		resultado = arg0.intValue() - arg1.intValue();
+		
+		return resultado;
+	}
+	
+	
+private int compareParidadeDistinta( Integer arg0, Integer arg1) {
+		
+		int resultado = 0;
+		
+		if(par(arg0)) {
+			resultado = -1;
+		} else {
+			resultado = 1;
+		}
+		
+		return resultado;
 	}
 
 }
