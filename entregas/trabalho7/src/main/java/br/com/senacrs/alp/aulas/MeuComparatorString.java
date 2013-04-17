877b68482677ffa8652912aaa427e317216372ba
631220041 <631220041@Sala603-12.fspoaeduc.com.br>
2013-04-16 21:46:38 -0300

    ok...

--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorString.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/MeuComparatorString.java
@@ -7,7 +7,11 @@ public class MeuComparatorString implements Comparator<String>{
 	@Override
 	public int compare(String o1, String o2) {
 		
-		return o2.compareTo(o1);
+		int resultado = 0;
+		
+		resultado = o2.compareTo(o1);
+		
+		return resultado;
 	
 	}
 
