308c06d1d85fb40ccf86440af8ddfa7dc25f19d1
631220041 <631220041@Sala603-11.fspoaeduc.com.br>
2013-04-25 21:39:59 -0300

    ok....

---
+++ entregas/exercicio_string/src/main/java/br/com/senacrs/alp/aulas/MeuInterpretadorStrings.java
@@ -0,0 +1,65 @@
+package br.com.senacrs.alp.aulas;
+
+public class MeuInterpretadorStrings implements InterpretadorStrings {
+
+	@Override
+	public TIPO_STRING lerString(String str) {
+		
+		TIPO_STRING resultado = null;
+		
+		if (str == null) {
+			
+			resultado = TIPO_STRING.MAL_FORMADA;
+		} else if(ehComentario(str)){
+			
+			resultado = TIPO_STRING.COMENTARIO;
+		} else if (ehBemFormada(str)) {
+			
+			resultado = TIPO_STRING.BEM_FORMADA;
+		}else {
+			resultado = TIPO_STRING.MAL_FORMADA;
+		}
+		
+		
+		
+		return resultado;
+	}
+
+	private boolean ehBemFormada(String str) {
+		boolean resultado = false;
+		
+		String[] strings = null;
+		
+		strings = str.split(SEPARADOR);
+		
+		if ((strings.length == 2) 
+				&& existeString(strings[0])
+				&& existeString(strings[1])){
+			
+			resultado = true;
+		}
+		
+		
+		
+		return resultado;
+	}
+
+	private boolean existeString(String string) {
+		
+		boolean resultado = false;
+		
+		resultado = (string != null) && !string.trim().isEmpty();
+		
+		return resultado;
+	}
+
+	private boolean ehComentario(String str) {
+		
+		boolean resultado = false;
+		
+		resultado = str.trim().startsWith(PREFIXO_COMENTARIO);
+		
+		return resultado;
+	}
+
+}
