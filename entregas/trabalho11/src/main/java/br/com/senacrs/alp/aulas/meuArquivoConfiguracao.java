79e8ab97ffa008f448931083d0d3798a83bd652f
alexnlv@hotmail.com <631220041@Sala603-16.fspoaeduc.com.br>
2013-06-04 19:47:49 -0300

    começou

---
+++ entregas/trabalho11/src/main/java/br/com/senacrs/alp/aulas/meuArquivoConfiguracao.java
@@ -0,0 +1,29 @@
+package br.com.senacrs.alp.aulas;
+
+public class meuArquivoConfiguracao implements ArquivoConfiguracao {
+	
+	
+	
+	public meuArquivoConfiguracao(String arquivoEntrada) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getRootDir() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getErrorDir() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPort() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
