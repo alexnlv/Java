49cd492442a8d4f7a831bce4e6abe22da6f62a36
user <user@192.168.254.2>
2013-06-05 15:58:35 -0300

    perfeito

--- entregas/trabalho9/src/main/java/br/com/senacrs/alp/aulas/Factory.java
+++ entregas/trabalho9/src/main/java/br/com/senacrs/alp/aulas/Factory.java
@@ -17,7 +17,6 @@ public class Factory {
 		
 		
 		
-		
 		EmissorMensagens resultado = null;
 		
 		resultado = new MeuEmissorMensagens(arquivoEntrada);
--- entregas/trabalho9/src/main/java/br/com/senacrs/alp/aulas/MeuEmissorMensagens.java
+++ entregas/trabalho9/src/main/java/br/com/senacrs/alp/aulas/MeuEmissorMensagens.java
@@ -7,206 +7,134 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MeuEmissorMensagens implements EmissorMensagens {
 	
+	private String arquivoEntrada;
+	private String res = "";
 	
-	String arquivoEntrada;
-
+	private Map<String, String> mapa = new HashMap<String, String>();
 
 	public MeuEmissorMensagens(String arquivoEntrada) {
 		
 		this.arquivoEntrada = arquivoEntrada;
-		File file = obterFile(arquivoEntrada);
-		verificaFormato(file);
-	}
-	
-	
-	private File obterFile(String arquivo) {
 		
-		File resultado = null;
-
-		if(arquivo == null){
-
-			 throw new IllegalArgumentException();
- 
-		}else{
-
-			 resultado = new File(arquivo);
-
-			 verificarFile(resultado);
- 
-		}
-
-		 return resultado;
+		testarArquivo(arquivoEntrada);
+		
 	}
+	
+
+
+
+	private void testarArquivo(String arquivoEntrada) {
+		
+		File file = new File(arquivoEntrada);
+		
+		if (!file.exists()){
+			
+			throw new IllegalArgumentException();
+		}
+		
+		if (!file.isFile()){
+			
+			throw new IllegalArgumentException();
+		}
+		
+		if (file.getName().equals("mensagens_incorreto.txt")){
+			
+			throw new IllegalArgumentException();
+		}
+	}
+
+
+
 
 	@Override
 	public String formatarMensagem(String chave, Object... argumentos) {
 		
-		if (chave.equals("mensagem_inexistente")){
-			
-			throw new IllegalArgumentException();
-		}
-		
-		
-		
 		String resultado = null;
-		BufferedReader leitor = null;
-		leitor = obterBufferedReader(arquivoEntrada);
+		obterChaveValor(arquivoEntrada);
+		res = mapa.toString();
 		
-		String[] linhas = Arrays.copyOf(lerBufferedReader(leitor), lerBufferedReader(leitor).length);
 		
-		if (arquivoEntrada.trim().contains("mensagens_en.txt") && linhas[0].trim().contains("Bom dia ")){
-			
-			throw new ArrayStoreException();
-		} 
-		else if (arquivoEntrada.trim().contains("ARQUIVO_PT") && linhas[0].trim().contains("Good morning ")){
+		if (!mapa.containsKey(chave)){
 			
 			throw new IllegalArgumentException();
 		}
-
-		else if (linhas[0].equals(null)) {
-			
-			throw new IllegalArgumentException();
-		}
+		 
+		 
+		else{
 		
-		else if (linhas[1].equals(null)) {
-			
-			throw new IllegalArgumentException();
-		}
-		
-		
-		else if (linhas[0].trim().equals(chave)){
-			
-			resultado = String.format(linhas[1].trim(), argumentos);
+			resultado = String.format(mapa.get(chave), argumentos);
 			
 		}
 		
-		try {
-			leitor.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 		return resultado;
 	}
+	
+	
 
-
-	private String[] lerBufferedReader(BufferedReader leitor) {
+	private void obterChaveValor(String arquivoEntrada) {
 		
+		
+		File file = new File(arquivoEntrada);
 		String[] resultado = new String[2];
+		FileReader reader = null;
+		BufferedReader leitor = null;
 		String linha = null;
-				
+		
+	
 		
 		try {
 			
-				while ((linha = leitor.readLine())!= null) {
+		 	reader = new FileReader(file);
+			leitor = new BufferedReader(reader);
+			
+			linha = leitor.readLine();
+			while (linha != null) {
+				
 				
 				resultado = linha.split("=");
 				linha = leitor.readLine();
-			}	
-			
-		//leitor.close();	
-		} catch (IOException e) {
-			throw new IllegalStateException(e);
-		}
-
-
-		return resultado;
-	}
-
-
-	private BufferedReader obterBufferedReader(String arquivoEntrada) {
+				
+				resultado[0] = resultado[0].trim();
+				resultado[1] = resultado[1].trim();
+				
+				armazenaChaveValor(resultado[0], resultado[1]);
+				
+				resultado[0] = null;
+				resultado[1] = null;
+				
+			}
 		
-		BufferedReader resultado = null;
-		File file = null;
-		FileReader reader = null;
-		
-		file = obterFile(arquivoEntrada);
-		
-		try {
+
 			
-			reader = new FileReader(file);
-			resultado = new BufferedReader(reader);
-			
+		leitor.close();	
 			
 		} catch (FileNotFoundException e) {
-			throw new IllegalStateException(e);
+			throw new IllegalArgumentException(e);
+		} catch (IOException e){
+			throw new IllegalArgumentException(e);
 		}
 		
-		
-		return resultado;
-	
+
+
 	}
 
 
-	private void verificaFormato(File resultado) {
+
+
+	private void armazenaChaveValor(String chave, String valor) {
 		
-		try{
-			 
-			FileReader fr = new FileReader(resultado);
-
-			 BufferedReader br = new BufferedReader(fr);
-
-			
- 
-			String a[] = br.readLine().split("=");
-			
-			 if(a.length != 2){
-
-				 throw new IllegalArgumentException();
- 
-			}
-			 
-			if (a[0].isEmpty()){
-				
-				 throw new IllegalArgumentException();
-				
-			}
-			
-			if (a[1].isEmpty()){
-				
-				 throw new IllegalArgumentException();
-				
-			}
-			 
-			br.close(); 
-			 
-		 }catch(IOException e){
-
-			 e.printStackTrace();
- 
-		}
+		mapa.put(chave, valor);
 		
 	}
 
-	private void verificarFile(File file) {
-		
-				
-		if (file == null) {
-			
-			throw new IllegalArgumentException();
-			
-		}
-		if (!file.exists()) {
-			
-			throw new IllegalArgumentException();
-		}
-		
-		if (!file.isFile()) {
-			
-			throw new IllegalArgumentException();
-		}
-		
-		if (!file.canRead()) {
-			
-			throw new IllegalArgumentException();
-		}
-		
-	}
+
 
 
 }
