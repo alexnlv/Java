52bcd18ad283e0fa8f780a76c686eaace0f671c1
user <user@192.168.254.2>
2013-06-07 01:26:08 -0300

    ok.......

--- entregas/trabalho11/src/main/java/br/com/senacrs/alp/aulas/meuArquivoConfiguracao.java
+++ entregas/trabalho11/src/main/java/br/com/senacrs/alp/aulas/meuArquivoConfiguracao.java
@@ -25,10 +25,10 @@ public class meuArquivoConfiguracao implements ArquivoConfiguracao {
 	private void leitura(String arquivoEntrada) {
 		
 		File file = new File(arquivoEntrada);
-		String[] resultado = new String[3];
 		FileReader reader = null;
 		BufferedReader leitor = null;
-		String linha = null;
+		String linha, chave, valor = null;
+		String[] chaveValor = new String[2];
 		
 	
 		
@@ -39,18 +39,44 @@ public class meuArquivoConfiguracao implements ArquivoConfiguracao {
 			
 			linha = leitor.readLine();
 			while (linha != null) {
-				
-					if(ehComentario(linha)){
-						
-					}else{
-						contemComentario(linha);				
-					}
 					
+				while(linha.contains("#")){
+					
+					linha = tratarComentarios(linha);
+					
+				} 
+				
+				if(linha.contains("=")){
+				
+					chaveValor = linha.split("=");
+				
+					if (chaveValor.length == 2){
+					
+						chave = chaveValor[0].trim();
+						valor = chaveValor[1].trim();
+		
+						mapa.put(chave, valor);
+						
+						testarMapa(mapa);
+						
+					} else {
+					
+						throw new IllegalArgumentException();
+					}
+				}
+					
+				linha = leitor.readLine();
+				
+				
 			}
 		
-
+			if(mapa.size() < 3){
+				
+				throw new IllegalArgumentException();
+			}
 			
-		leitor.close();	
+			leitor.close();
+			
 			
 		} catch (FileNotFoundException e) {
 			throw new IllegalArgumentException(e);
@@ -61,26 +87,64 @@ public class meuArquivoConfiguracao implements ArquivoConfiguracao {
 		
 	}
 
-	private void contemComentario(String linha) {
-		if(linha.contains("#")){
-			String l[] = linha.split("#");
+	private void testarMapa(Map<String, String> mapa) {
+		
+		if(mapa.containsKey("root_dir")){
+			
+			if (mapa.get("root_dir").startsWith("./"));
+			else {
+				
+				throw new IllegalArgumentException();
+			}
 		}
+		
+		if(mapa.containsKey("error_dir")){
+			
+			if (mapa.get("error_dir").startsWith("./"));
+			else {
+				
+				throw new IllegalArgumentException();
+			}
+			
+		}
+		
+		if(mapa.containsKey("port")){
+			
+			if (Integer.parseInt(mapa.get("port")) >= 1000 );
+			else {
+				
+				throw new IllegalArgumentException();
+			}
+			
+		}
+		
+		
 	}
 
-	private boolean ehComentario(String linha) {
-		boolean resultado = false;
-		if(!linha.startsWith("#")){
-			resultado = false;
-		}else{
-			resultado = true;
+	private String tratarComentarios(String linha) {
+		
+		String resultado = null;
+		
+		if(linha.startsWith("#")){
+			resultado = "";
+			
+		} else {
+			
+			String[] aux = linha.split("#");
+			
+			if (aux.length == 2){
+				
+				resultado = aux[0].trim();
+		
+			} else {
+				
+				throw new IllegalArgumentException();
+			}
+				
+			
 		}
+		
 		return resultado;
-	}
-
-	private void armazenaChaveValor(String chave, String valor) {
-		
-		mapa.put(chave, valor);
-		
 	}
 
 	private void testarArquivo(String arquivoEntrada) {
@@ -101,19 +165,51 @@ public class meuArquivoConfiguracao implements ArquivoConfiguracao {
 	@Override
 	public String getRootDir() {
 		
+		String resultado = null;
 		
-		return null;
+		if (mapa.containsKey("root_dir")){
+			
+			if (mapa.get("root_dir").startsWith("./")){
+				
+				resultado = mapa.get("root_dir");
+			
+			} else {
+				
+				throw new IllegalArgumentException();
+			}
+			
+		}
+		
+		
+		return resultado;
 	}
 
 	@Override
 	public String getErrorDir() {
-		return null;
+		
+		String resultado = null;
+		
+		if (mapa.containsKey("error_dir")){
+			
+			resultado = mapa.get("error_dir");
+		}
+		
+		return resultado;
 	}
 
 	@Override
 	public int getPort() {
 		
-		return 0;
+		int resultado = 0;
+		
+		if (mapa.containsKey("port")){
+			
+			resultado = Integer.parseInt(mapa.get("port"));
+		}
+		
+		
+		
+		return resultado;
 	}
 
 }
